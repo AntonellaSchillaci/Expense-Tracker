dc42c536838365400638b1a3ffeee7eaac27164e
Antonella Schillaci <antonella.schillaci145@gmail.com>
2025-07-21 09:58:28 +0200

    feat(expense): add core functionalities in expense.service and add expense.model

---
+++ src/app/services/expense.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from "@angular/core";
+import { Expense } from "../models/expense.model";
+
+
+@Injectable ({
+    providedIn: 'root',
+})
+export class ExpenseService {
+    private readonly STORAGE_KEY = 'expenses';
+    private expenses: Expense[] = [];
+
+    constructor() {
+        this.loadFromStorage();
+    }
+
+    private loadFromStorage() : void {
+        const data = localStorage.getItem(this.STORAGE_KEY);
+        if(data) {
+            this.expenses = JSON.parse(data);
+        }
+    }
+
+    private saveToStorage() : void {
+        localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.expenses));
+    }
+
+    getExpenses() : Expense[] {
+        return [...this.expenses];
+    }
+
+    addExpense(expense: Expense) : void {
+        this.expenses.push(expense);
+        this.saveToStorage();
+    }
+
+    removeExpense(id: number) : void {
+        this.expenses = this.expenses.filter (e => e.id !== id);
+        this.saveToStorage();
+    }
+    getTotalAmount() : number {
+        return this.expenses.reduce((sum, e) => sum + e.amount, 0);
+    }
+
+}
